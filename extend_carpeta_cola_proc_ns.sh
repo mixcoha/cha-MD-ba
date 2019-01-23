cd632fddc6e29e52284799e788f334f9b8ef44b8
Edgar Mixcoha <mixcoha@users.noreply.github.com>
2019-01-23 14:40:58 -0600

    extend_carpeta_cola_proc_atomic.sh
    Script para enviar en Yoltla extensiones de dinámica molecular en simulaciones con archivos con la estructura : $RUNDIR/$1/R1/prod/

--- extend_carpeta_cola_proc_ns.sh
+++ extend_carpeta_cola_proc_ns.sh
@@ -11,15 +11,18 @@ RUNDIR=$PWD
 
 cd $RUNDIR/$1/R1/prod
 
-                ls -lhtr > ls
-                grep drwxr ls > tmp
-                cat tmp | sort -k 9 -g > tmp1
-                mv tmp1 tmp
-                nd=`awk 'END {print $NF+1}' tmp`
-                rm ls tmp
-                mkdir $nd
-                mv *.* $nd
-                cd $nd
+ls -lhtr > ls
+grep drwxr ls > tmp
+cat tmp | sort -k 9 -g > tmp1
+mv tmp1 tmp
+nd=`awk 'END {print $NF+1}' tmp`
+rm ls tmp
+mkdir $nd
+mv *.* $nd
+cd $nd
+
+node=$(( $3/10 ))
+ntask=$(( $3/5 ))
 
 cd $RUNDIR/
 
@@ -30,7 +33,10 @@ cat > ext_${nd}_$1_en$2_$3p_$4ns.slmr << estodo
 #SBATCH -n $3
 #SBATCH -e ${nd}_$1_error_%J
 #SBATCH -o ${nd}_$1salida_%J
-
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=5 
+#SBATCH --nodes=$node
+#SBATCH --ntasks=$ntask
 
 cd $RUNDIR/$1/R1/prod
 
@@ -39,13 +45,7 @@ module load intel/15.6.232/impi/5.0.3.49/gromacs/5.1.4-s
 
 export OMP_NUM_THREADS=5
 
-# Only 20 instances, 4 process and 5 threads per process: recommend for this version.
-# mpirun -n 4 gmx mdrun -v -deffnm file
-
-# > 20 processes
-task=$(( SLURM_NNODES * 4 ))
-srun -n $task hostname -s | sort > hostlist.dat
-mpirun -np 4 -hostfile ./hostlist.dat gmx mdrun -v -cpi >& mdrun.log
+mpiexec.hydra gmx mdrun -v -cpi $RUNDIR/$1/R1/prod/$nd/state.cpt -noappend >& mdrun.log
 
 exit 0
 
